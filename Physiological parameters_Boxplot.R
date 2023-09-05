4a28875c05eddd01967bf4d77f901c44713aeade
Fabio-Trevisan <98732667+Fabio-Trevisan@users.noreply.github.com>
2023-09-05 15:54:27 +0200

    Added script for side by side boxplot + graphs

--- Physiological parameters_Boxplot.R
+++ Physiological parameters_Boxplot.R
@@ -7,28 +7,37 @@ library(dplyr)
 library(agricolae)
 library(readxl)
 library(rstatix)
-
+library(ggbreak)
+library(scales)
 
 
 
 #read csv & rename columns ####
 table <- read_excel("DATA_Physiological_Parameters.xlsx", sheet = "1")
-name <- "Physiological_Parameters"
+name <- "Physiological parameters"
+table <- table[,-5]
 
-
-
-
-colnames(table) <- c("Treatment", "E", "A", "gs", "Normalized gs")
+colnames(table) <- c("Treatment", "E", "A", "gs")
 
 
 
 #melt & group & remove missing values ####
 table1 <- melt(table, id=c("Treatment"))
-
 table_1_sum<-table1 %>% 
   group_by(variable)
-
 Table_2_sum <- table_1_sum %>% drop_na(value)
+Table_2_sum <- table_1_sum[table_1_sum$`value`>0,]
+
+
+df <- table
+df$A_mol <- df$A/10^6
+df <- df[-3]
+df <- melt(df, id=c("Treatment"))
+df_sum<-df %>% 
+  group_by(variable)
+df_sum2 <- df_sum %>% drop_na(value)
+df_sum2 <- df_sum[df_sum$`value`>0,]
+
 
 
 #boxplots & facet wrap ####
@@ -38,7 +47,7 @@ ggplot(Table_2_sum, aes(x= Treatment, value, fill=Treatment))+
   geom_boxplot(width=0.5)+ 
   labs(caption ="(based on ADC instruement. Fabio Trevisan)")+ 
   theme(legend.position = "NONE") + theme_bw() +
-  facet_wrap(~variable, scales="free") + ylab(name) + xlab("Treatments") +
+  facet_wrap(~variable, scales="free", nrow = 2) + ylab("mol m-²  s-1") + xlab("Treatments") +
   scale_x_discrete(limits=my_order) +
   scale_fill_manual(values=c("grey77","darkorange2", "skyblue3"))
 
@@ -46,10 +55,43 @@ ggsave(filename = paste(name, ".pdf", sep = ""), plot = last_plot(), dpi = 600, 
 
 
 
+#side by side boxplots ####
+my_order<- c("A_mol", "E", "gs")
+f1 <- ggplot(df_sum2, aes(x = variable, y = value, fill=Treatment))+  
+  stat_boxplot(geom="errorbar", width=0.2, position = position_dodge(width = 0.5))+
+  geom_boxplot(width=0.5)+ 
+  theme(legend.position = "NONE") + theme_bw() + 
+  scale_fill_manual(values=c("grey77","darkorange2", "skyblue3")) +
+  scale_x_discrete(limits=my_order) +
+  ylab("mol m-²  s-1") + xlab("Physiological parameters") 
+  
+f2 <- f1 + 
+  scale_y_break(c(0.04,0.08),  scales = 1) +
+  scale_y_break(c(0.0028,0.0000025),  scales = 1) +
+  scale_y_continuous(breaks = seq(0, 1 , 0.1), labels = scientific) 
+f2
+ggsave(filename = paste(name, "_1.pdf", sep = ""), plot = f2, dpi = 600, units = "cm", width = 60, height = 50, scale = 0.5)
+
+f3 <- f1 + 
+  scale_y_break(c(0.04,0.08),  scales = 1) +
+  scale_y_break(c(0.0028,0.0000025),  scales = 1) +
+  scale_y_continuous(breaks = seq(0, 0.04 , 0.01), labels = scientific) 
+f3
+ggsave(filename = paste(name, "_2.pdf", sep = ""), plot = f3, dpi = 600, units = "cm", width = 60, height = 50, scale = 0.5)
+
+f4 <- f1 + 
+  scale_y_break(c(0.04,0.08),  scales = 1) +
+  scale_y_break(c(0.0028,0.0000025),  scales = 1) +
+  scale_y_continuous(breaks = seq(0, 0.0000025 , 0.0000005), labels = scientific) 
+f4
+ggsave(filename = paste(name, "_3.pdf", sep = ""), plot = f4, dpi = 600, units = "cm", width = 60, height = 50, scale = 0.5)
+
+
+
 #statistics ####
 colnames(Table_2_sum) <- c("Treatment", "Variable", "Value") 
 
-#Assumptions 
+##Assumptions ####
 ## 1. Homogeneity of variances
 ##Treatment*Time
 Table_2_sum$Treatment <- factor(Table_2_sum$Treatment)
@@ -61,7 +103,6 @@ L_test <- Table_2_sum %>%
 View(L_test)
 write.table(L_test, file = "Physiological_Parameters_Levene_test_results.csv", quote = FALSE, sep = ";")
 
-
 ##2. Normality
 ##Shapiro-Wilk test for all single treatments
 SW_test <- Table_2_sum %>%
@@ -71,11 +112,11 @@ View(SW_test)
 write.table(SW_test, file = "Physiological_Parameters_ShapiroWilk_test_results.csv", quote = FALSE, sep = ";")
 
 ##3. Indipendency
-Data are indepent by experimental design!
+#Data are indepent by experimental design!
 
 
 
-##anova for tukey
+##anova for tukey ####
 OneWay_Anova_Boxplot <- lapply(split(Table_2_sum, Table_2_sum[["Variable"]]), function(i){ 
   aov(Value ~ Treatment, data = i)
 })
@@ -89,7 +130,7 @@ sink(paste(name, "OneWay_Anova_Boxplot.csv", sep = "_"))
 OneWay_Anova_Boxplot2
 sink(NULL)
 
-#Tukey
+##Tukey ####
 ##HSD complete
 HSD_Boxplot <- lapply(names(OneWay_Anova_Boxplot), function(i){ 
   HSD.test(OneWay_Anova_Boxplot[[i]], "Treatment")
